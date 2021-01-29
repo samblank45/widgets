ea982796e4afa07e7c138c010dd8d6c3a5323a2a
sam <sblan45@gmail.com>
2021-01-28 22:39:21 -0500

    adds accordion widget and simple questions inside

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use react by creating compenents'
+  }
+]
 
 class App extends React.Component {
   render() {
-    return <div>hello</div>
+    return (
+      <div>
+        <Accordion items={items}/>
+      </div>
+    )
   }
 }
 
