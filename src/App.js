c5b160eeb5f7d9bd6f52aceb4c013c9290838552
sam <sblan45@gmail.com>
2021-02-08 18:14:03 -0500

    adds Search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accordion from './components/Accordion'
+import Search from './components/Search'
 
 const items = [
   {
@@ -20,7 +21,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Accordion items={items}/>
+        {/* <Accordion items={items}/> */}
+        <Search />
       </div>
     )
   }
