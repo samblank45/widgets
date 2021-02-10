22af3f57e9727511f302646cc24032dba5087d3c
sam <sblan45@gmail.com>
2021-02-09 21:02:56 -0500

    adds search bar with event handler

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Search = () => {
-  return <h1>Search</h1>
+  const [term, setTerm] = useState('')
+
+  useEffect(function() {
+    console.log('ljlskjdf')
+  })
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input 
+            className="input" 
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search
