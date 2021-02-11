e5bab41d36c9813d13fad650ffaa254bd55c509b
sam <sblan45@gmail.com>
2021-02-10 22:14:33 -0500

    adds axios get request to wikipedia and displays results on page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,41 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('')
+  const [results, setResults] = useState([])
 
-  useEffect(function() {
-    console.log('ljlskjdf')
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+      setResults(data.query.search)
+    }
+    if (term) {
+      search()
+    }
+  }, [term])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div>
+        <div key={result.pageid} className="item">
+          <div className="content">
+            <div className="header">
+              {result.title}
+            </div>
+            {result.snippet}
+          </div>
+        </div>
+      </div>
+    )
   })
 
   return (
@@ -18,6 +49,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
