a2cae57f7fe47065d21236878b06d5b3bb3e35a7
sam <sblan45@gmail.com>
2021-04-23 22:17:37 -0400

    adds header and route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import DropDown from './components/DropDown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 
 
 const items = [
@@ -35,38 +37,56 @@ const options = [
   }
 ]
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />
-  }
-}
+// const showAccordion = () => {
+//   if (window.location.pathname === '/') {
+//     return <Accordion items={items} />
+//   }
+// }
 
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />
-  }
-}
+// const showList = () => {
+//   if (window.location.pathname === '/list') {
+//     return <Search />
+//   }
+// }
 
-const showDropDown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <DropDown />
-  }
-}
+// const showDropDown = () => {
+//   if (window.location.pathname === '/dropdown') {
+//     return <DropDown />
+//   }
+// }
 
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />
-  }
-}
+// const showTranslate = () => {
+//   if (window.location.pathname === '/translate') {
+//     return <Translate />
+//   }
+// }
+
+// const showComponent = (route, component) => {
+//   return window.location.pathname === route ? component : null;
+// }
 
 export default () => {
-
+  const [selected, setSelected] = useState(0)
   return  (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropDown()}
-      {showTranslate()}
+      <Header />
+      <Route path="/">
+        <Accordion items={items}/>
+      </Route>
+      <Route path="/list">
+        <Search/>
+      </Route>
+      <Route path="/dropdown">
+        <DropDown 
+          label="select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   )
 }
